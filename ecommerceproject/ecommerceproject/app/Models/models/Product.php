2fcd1af503c645871229b588b1641e090a2fc8c5
Shakil Ahmed <68857266+shakilahmed80@users.noreply.github.com>
2021-04-28 20:26:30 +0600

    Update Product.php

--- ecommerceproject/ecommerceproject/app/Models/models/Product.php
+++ ecommerceproject/ecommerceproject/app/Models/models/Product.php
@@ -12,7 +12,7 @@ class Product extends Model
 
     public function product_category(){
 
-        return $this->belongsTo('App\models\ProductCtegory');
+        return $this->belongsTo('App\models\ProductCategory');
 
     }
 
